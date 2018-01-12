fc961238e294bc52bce478b17985943e919c2017
Serge Rybakov <serge_rybakov@senla.eu>
2018-01-12 16:23:01 +0300

    added, updated

--- lua/Grand Master Yoda.lua
+++ lua/Grand Master Yoda.lua
@@ -13,9 +13,9 @@ if cnt > 0 and cnt%size == 0 then
 	print(">>> Found", cnt, "results.")
 	local r = gg.getResults(cnt)
 	for i=0, cnt/size-1 do
-		--r[1+i*size].value = 100000 -- health
+		r[1+i*size].value = 100000 -- health
 		r[2+i*size].value = 10000 -- speed
-		r[3+i*size].value = 200000 -- basic damage
+		r[3+i*size].value = 500000 -- physical damage
 	end
 	gg.setValues(r)
 	print(">>> Done.")
--- lua/Nightsister Acolyte.lua
+++ lua/Nightsister Acolyte.lua
@@ -1,24 +1,27 @@
 --gg.setVisible(false)
 
-print("Finn 10%")
+print("Nightsister Acolyte")
 
 gg.clearResults()
 gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_BSS)
 
-gg.searchNumber("13352;107;1148::100", gg.TYPE_QWORD)
+gg.searchNumber("9853;439;121;1013::100", gg.TYPE_QWORD)
 
-local size = 3
+local size = 4
 local cnt = gg.getResultCount()
 if cnt > 0 and cnt%size == 0 then
 	print(">>> Found", cnt, "results.")
 	local r = gg.getResults(cnt)
 	for i=0, cnt/size-1 do
 		r[1+i*size].value = 100000 -- health
-		r[2+i*size].value = 10000 -- speed
-		r[3+i*size].value = 500000 -- physical damage
+		r[2+i*size].value = 4390 -- agility
+		r[3+i*size].value = 10000 -- speed
+		r[4+i*size].value = 500000 -- physical damage
 	end
 	gg.setValues(r)
 	print(">>> Done.")
 else
-	print("!!! Strange number of results: ", cnt)
+	print(">>> Strange number of results: ", cnt)
 end
+
+
