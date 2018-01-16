0d9000887ea8a4e6493b54f8b0ad098b72acebe1
Serge Rybakov <>
2018-01-17 01:50:15 +0300

    add, update

--- lua/Snowtrooper.lua
+++ lua/Snowtrooper.lua
@@ -5,7 +5,7 @@ print("Snowtrooper 5%")
 gg.clearResults()
 gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_BSS)
 
-gg.searchNumber("12917;86;1175::100", gg.TYPE_QWORD)
+gg.searchNumber("12917;86;1235::100", gg.TYPE_QWORD)
 
 local size = 3
 local cnt = gg.getResultCount()
@@ -15,7 +15,7 @@ if cnt > 0 and cnt%size == 0 then
 	for i=0, cnt/size-1 do
 		r[1+i*size].value = 100000 -- health
 		r[2+i*size].value = 10000 -- speed
-		r[3+i*size].value = 500000 -- basic damage
+		r[3+i*size].value = 500000 -- physical damage
 	end
 	gg.setValues(r)
 	print(">>> Done.")
