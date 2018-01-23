85788ea3d917baed660b9bac6d3f44649afcabd5
Serge Rybakov <sergey@MacBook-Pro-Serge.local>
2018-01-23 17:26:30 +0300

    added, updated

--- lua/CT-21-0408 Echo.lua
+++ lua/CT-21-0408 Echo.lua
@@ -1,11 +1,11 @@
 --gg.setVisible(false)
 
-print("CT-7567 Rex 5%")
+print("CT-21-0408 Echo 5%")
 
 gg.clearResults()
 gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_BSS)
 
-gg.searchNumber("15429;199;2336::100", gg.TYPE_QWORD)
+gg.searchNumber("8271;111;884::100", gg.TYPE_QWORD)
 
 local size = 3
 local cnt = gg.getResultCount()
