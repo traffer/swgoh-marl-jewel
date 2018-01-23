0dc049dbcc80d9039ed5ed940441148b8b2d4590
Serge Rybakov <>
2018-01-23 20:43:18 +0300

    add, update

---
+++ lua/Ewok Scout.lua
@@ -0,0 +1,24 @@
+--gg.setVisible(false)
+
+print("Ewok Scout 5%")
+
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_BSS)
+
+gg.searchNumber("8313;128;1280::100", gg.TYPE_QWORD)
+
+local size = 3
+local cnt = gg.getResultCount()
+if cnt > 0 and cnt%size == 0 then
+	print(">>> Found", cnt, "results.")
+	local r = gg.getResults(cnt)
+	for i=0, cnt/size-1 do
+		r[1+i*size].value = 100000 -- health
+		r[2+i*size].value = 10000 -- speed
+		r[3+i*size].value = 500000 -- physical damage
+	end
+	gg.setValues(r)
+	print(">>> Done.")
+else
+	print("!!! Strange number of results: ", cnt)
+end
--- lua/Geonosian Soldier.lua
+++ lua/Geonosian Soldier.lua
@@ -13,12 +13,12 @@ if cnt > 0 and cnt%size == 0 then
 	print(">>> Found", cnt, "results.")
 	local r = gg.getResults(cnt)
 	for i=0, cnt/size-1 do
-		--r[1+i*size].value = 150000 -- health
-		r[2+i*size].value = 100000 -- speed
-		r[3+i*size].value = 500000 -- basic damage
+		r[1+i*size].value = 100000 -- health
+		r[2+i*size].value = 10000 -- speed
+		r[3+i*size].value = 500000 -- physical damage
 	end
 	gg.setValues(r)
 	print(">>> Done.")
 else
-	print(">>> Strange number of results: ", cnt)
+	print("!!! Strange number of results: ", cnt)
 end
