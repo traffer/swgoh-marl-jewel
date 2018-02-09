46b7ac00157d258fde87e8896ad40c3fe366319a
Serge Rybakov <sr@local>
2018-02-09 12:52:13 +0300

    updated

--- lua/ships/Gauntlet Starfighter.lua
+++ lua/ships/Gauntlet Starfighter.lua
@@ -5,7 +5,7 @@ print("Gauntlet Starfigher")
 gg.clearResults()
 gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_BSS)
 
-gg.searchNumber("70732;142;8932::100", gg.TYPE_QWORD)
+gg.searchNumber("72556;144;9218::100", gg.TYPE_QWORD)
 
 local size = 3
 local cnt = gg.getResultCount()
@@ -15,7 +15,7 @@ if cnt > 0 and cnt%size == 0 then
 	for i=0, cnt/size-1 do
 		--r[1+i*size].value = 150000 -- health
 		r[2+i*size].value = 10000 -- speed
-		r[3+i*size].value = 500000 -- basic damage
+		r[3+i*size].value = 500000 -- physical offense
 	end
 	gg.setValues(r)
 	print(">>> Done.")
