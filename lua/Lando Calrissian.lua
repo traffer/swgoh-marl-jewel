24d432145dd5355d26ad7a4635a774e17936f9e5
Serge Rybakov <sr@local>
2018-02-10 18:11:54 +0300

    added, updated

--- lua/Lando Calrissian.lua
+++ lua/Lando Calrissian.lua
@@ -1,11 +1,11 @@
 --gg.setVisible(false)
 
-print("Endurance")
+print("Lando Calrissian 15%")
 
 gg.clearResults()
 gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_BSS)
 
-gg.searchNumber("118;4973;3105::100", gg.TYPE_QWORD)
+gg.searchNumber("16238;166;2266::100", gg.TYPE_QWORD)
 
 local size = 3
 local cnt = gg.getResultCount()
@@ -13,9 +13,9 @@ if cnt > 0 and cnt%size == 0 then
 	print(">>> Found", cnt, "results.")
 	local r = gg.getResults(cnt)
 	for i=0, cnt/size-1 do
-		r[1+i*size].value = 1500 -- speed
-		r[2+i*size].value = 94000 -- physical offense
-		r[3+i*size].value = 94000 -- special offense
+		r[1+i*size].value = 100000 -- health
+		r[2+i*size].value = 10000 -- speed
+		r[3+i*size].value = 500000 -- physical damage
 	end
 	gg.setValues(r)
 	print(">>> Done.")
