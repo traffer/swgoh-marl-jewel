0cfa82caa8db987c80cf3b00754e050351cefce9
Serge Rybakov <sr@local>
2018-05-23 17:00:07 +0300

    no message

--- lua/Alpha/HAAT/haat_spd.lua
+++ lua/Alpha/HAAT/haat_spd.lua
@@ -71,7 +71,7 @@ for k,v in pairs(choice) do
 		for i=0, cnt/size-1 do
 			--r[1+i*size].value = 10000 -- health
 			r[2+i*size].value = 50 + r[2+i*size].value -- speed
-			--r[3+i*size].value = 0 + r[3+i*size].value -- physical damage
+			r[3+i*size].value = 2000 + r[3+i*size].value -- physical damage
 		end
 		resObj.table = r
 	else
